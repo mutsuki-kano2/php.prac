e2d6ab8aed32538b4510b49472937229da49e5ef
mutsuki-kano2 <kano@genz.jp>
2024-12-11 21:25:27 +0900

    Create prac3.php

---
+++ prac3.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>何曜日？</title></head>
+<body>
+曜日を知りたい日にちをxxxxxxxx（半角数字8ケタ）で入力してください。
+<br>
+<form action="week2.php" method="post">
+<input type="text" name="date">
+<input type="submit" value="送信">
+</form>
+</ｂody>
+</html>
